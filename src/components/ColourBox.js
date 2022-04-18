fb7f611a06705d486ae0cfa7ea9ff92f528eb62f
Stewart MacLeman <stewart_macleman@hotmail.com>
2022-04-18 22:47:18 +0100

    README.md added, style updates.

--- src/components/ColourBox.js
+++ src/components/ColourBox.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const ColourBox = (props) => {
-    return (
-        <div className="colourBox" style={{ backgroundColor: `${props.boxColour}` }}>
-            <p>Colour: {props.boxText}</p>
-        </div>
-    )
-}
+  return (
+    <div
+      className="colourBox"
+      style={{ backgroundColor: `${props.boxColour}` }}
+    >
+      <p>Colour: {props.boxText}</p>
+    </div>
+  );
+};
 
 export default ColourBox;
